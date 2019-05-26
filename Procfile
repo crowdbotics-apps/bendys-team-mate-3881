56850ac2ff4ae2a87f0c96a896dc393dc715474c
Crowdbotics <team@crowdbotics.com>
2019-05-26 22:54:31 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: waitress-serve --port=$PORT backend.bendys_team_mate_3881.wsgi:application
+release: ./backend/release-tasks.sh
---
+++ backend/bendys_team_mate_3881/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bendys_team_mate_3881 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bendys_team_mate_3881.settings")
+
+application = get_wsgi_application()
